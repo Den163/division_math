098c675f68220567427d8de351f4b7004522e102
den163 <abramov.denis163@gmail.com>
2023-04-16 01:32:49 +0400

    Added matrix to scalar mul

--- src/matrix4x4.rs
+++ src/matrix4x4.rs
@@ -7,10 +7,14 @@ pub struct Matrix4x4 {
 }
 
 impl Matrix4x4 {
+    #[inline]
     pub fn from_columns(columns: [Vector4; 4]) -> Matrix4x4 { Matrix4x4 { columns } }
+
+    #[inline]
     pub fn all(v: f32) -> Matrix4x4 { Matrix4x4 { columns: [Vector4::all(v); 4] } }
 
-    pub fn identity() -> Self {
+    #[inline]
+    pub fn identity() -> Matrix4x4 {
         Matrix4x4 {
             columns: [
                 Vector4::new(1., 0., 0., 0.),
@@ -21,8 +25,10 @@ impl Matrix4x4 {
         }
     }
 
-    pub fn zero() -> Self { Matrix4x4 { columns: [Vector4::zero(); 4] } }
+    #[inline]
+    pub fn zero() -> Matrix4x4 { Matrix4x4 { columns: [Vector4::zero(); 4] } }
 
+    #[inline]
     pub fn ortho(left: f32, right: f32, bottom: f32, top: f32, near: f32, far: f32) -> Self {
         Matrix4x4 {
             columns: [
@@ -43,19 +49,22 @@ impl Matrix4x4 {
 impl Index<usize> for Matrix4x4 {
     type Output = Vector4;
 
+    #[inline]
     fn index(&self, index: usize) -> &Vector4 { &self.columns[index] }
 }
 
 impl IndexMut<usize> for Matrix4x4 {
+    #[inline]
     fn index_mut(&mut self, index: usize) -> &mut Self::Output { &mut self.columns[index] }
 }
 
-impl Mul for Matrix4x4 {
+impl Mul<f32> for Matrix4x4 {
     type Output = Matrix4x4;
 
-    fn mul(self, rhs: Self) -> Self::Output {
-        Matrix4x4::from_columns(
-            [self[0] * rhs[0], self[1] * rhs[1], self[2] * rhs[2], self[3] * rhs[3]]
-        )
+    #[inline]
+    fn mul(self, rhs: f32) -> Self::Output {
+        Matrix4x4::from_columns([
+            self[0] * rhs, self[1] * rhs, self[2] * rhs, self[3] * rhs
+        ])
     }
 }
